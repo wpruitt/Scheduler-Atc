a9efb7b467d2b3fd7f51ec6c8397628290bb78fc
Willie Pruitt <w_pruitt@hotmail.com>
2017-09-20 22:06:59 -0500

    TOR adding, editing, approval all works

---
+++ ATCSchedulerApp/Models/ViewModels/TORApprovalViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ATCScheduler.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATCScheduler.Models.ViewModels
+{
+    public class TORApprovalViewModel
+    {
+        public IEnumerable<TimeOffRequest> PendingTORs { get; set; }
+
+        public IEnumerable<TimeOffRequest> ApprovedTORs { get; set; }
+
+        public IEnumerable<TimeOffRequest> ConfirmedTORs { get; set; }
+
+        public IEnumerable<TimeOffRequest> DeniedTORs { get; set; }
+
+        public TORApprovalViewModel(ApplicationDbContext context, string UserId)
+        {
+            PendingTORs = (from pt in context.TimeOffRequest
+                           where pt.TORStatus == TimeOffRequest.Status.Pending
+                           select pt).Include("User");
+            ApprovedTORs = (from at in context.TimeOffRequest
+                            where at.TORStatus == TimeOffRequest.Status.Approved
+                            select at).Include("User");
+            ConfirmedTORs = (from ct in context.TimeOffRequest
+                            where ct.TORStatus == TimeOffRequest.Status.Confirmed
+                            select ct).Include("User");
+            DeniedTORs = (from dt in context.TimeOffRequest
+                            where dt.TORStatus == TimeOffRequest.Status.Denied
+                            select dt).Include("User");
+        }
+    }
+}
---
+++ ATCSchedulerApp/Models/ViewModels/TORListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATCScheduler.Models.ViewModels
+{
+    public class TORListViewModel
+    {
+        public IEnumerable<TimeOffRequest> TORs { get; set; }
+    }
+}
